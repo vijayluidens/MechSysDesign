2526a681b63afb4146c8fd87d9cd276c1b50cc66
vijayluidens <vijayluidens@gmail.com>
2024-11-21 10:46:57 +0100

    Folder Assignment 1 added

---
+++ Assignment 1/Question_A.m
@@ -0,0 +1,27 @@
+% System Parameters
+m1 = 2; %kg
+m2 = 0.2; %kg
+m3 = 0.05; %kg
+k1 = 10e3; %N/m
+k2 = 30e3; %N/m
+k3 = 40e3; %N/m
+c1 = 0.1; %Ns/m
+c2 = 0.1; %Ns/m
+c3 = 0.1; %Ns/m
+
+% Mass matrix
+M = [m1 0 0;
+    0 m2 0;
+    0 0 m3];
+
+% Damping matrix
+C = [c1+c2 -c2 0;
+    -c2 c2+c3 -c3;
+    0 -c3 c3];
+
+%Stiffness matrix
+K = [k1+k2 -k2 0;
+    -k2 k2+k3 -k3
+    0 -k3 k3];
+
+
