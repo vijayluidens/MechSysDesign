35f3a6a13830478c5fcea5ce5239db88da683242
vijayluidens <vijayluidens@gmail.com>
2024-11-22 13:56:11 +0100

    Transfer Functions Plotted

--- Assignment 1/Question_A.m
+++ Assignment 1/Question_A.m
@@ -1,14 +1,7 @@
-% System Parameters
-m1 = 2; %kg
-m2 = 0.2; %kg
-m3 = 0.05; %kg
-k1 = 10e3; %N/m
-k2 = 30e3; %N/m
-k3 = 40e3; %N/m
-c1 = 0.1; %Ns/m
-c2 = 0.1; %Ns/m
-c3 = 0.1; %Ns/m
-
+% Given Parameters
+m1 = 2; m2 = 0.2; m3 = 0.05; % Mass [kg]
+k1 = 10e3; k2 = 30e3; k3 = 40e3; % Spring Constant [N/m]
+c1 = 0.1; c2 = 0.1; c3 = 0.1; % Damping constant [Ns/m]
 
 % Mass matrix
 M = [m1 0 0;
@@ -25,9 +18,36 @@ K = [k1+k2 -k2 0;
     -k2 k2+k3 -k3
     0 -k3 k3];
 
-%Eigenmatrix
+% Frequency range for plotting 
+f = logspace(-1, 3, 1001); % Linear Frequency
+w = f * 2 * pi; % Angular Frequency
 
-% This is an updated version
+% Initialize transfer function H(s) = X(s) / F(s)
+H_s = zeros(3, 2, length(w));
 
+for i = 1:length(w)
+    s = 1j * w(i); % Complex frequency
+    A = M * s^2 + C * s + K; % System dynamics in Laplace
+    A_inv = inv(A); % Inverse of system dynamics
+    H_s(:, :, i) = A_inv(:, 1:2);  % Extract the first two columns (F1 and F2)
+end
+
+% Magnitudes of transfer functions for plotting
+H_magnitudes = abs(H_s);
+
+% Plot transfer functions (magnitude vs frequency in Hz)
+figure;
+for i = 1:3  % Rows for x1, x2, x3
+    for j = 1:2  % Columns for F1, F2
+        subplot(3, 2, (i - 1) * 2 + j);
+        loglog(f, squeeze(H_magnitudes(i, j, :)));
+        xlabel('Frequency (Hz)');
+        ylabel('|H(j\omega)|');
+        title(['Transfer Function H(', num2str(i), ',', num2str(j), ')']);
+        grid on;
+    end
+end
+
+sgtitle('Transfer Function Matrix H(s) (Frequency in Hz)');
 
 
